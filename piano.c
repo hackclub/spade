1edacc1af9e93bc668b2eed6c46535a8e32b757c
Kognise <felix.mattick@gmail.com>
2022-11-03 08:21:05 -0400

    Push my fucking code

--- piano.c
+++ piano.c
@@ -28,17 +28,13 @@ typedef enum {
 typedef struct {
   uint32_t step;
   Sound sound;
-  int sample_duration;
-} i2sNote;
-static i2sNote i2snote_sound(int freq, int duration_ms, Sound sound) {
-  return (i2sNote) {
+  uint32_t pos;
+} Note;
+static Note i2snote_sound(int freq, Sound sound) {
+  return (Note) {
     .sound = sound,
     .step = (freq * TABLE_LEN) / SAMPLES_PER_SECOND * 0x10000,
-    .sample_duration = (SAMPLES_PER_SECOND / 1000) * duration_ms,
   };
-}
-static i2sNote i2snote_pause(int duration_ms) {
-  return i2snote_sound(0, duration_ms, 0);
 }
 
 typedef struct {
@@ -46,15 +42,16 @@ typedef struct {
   double times;
 
   NoteReadState nrs;
-  void *song;
+  void *char_source;
 
-  uint32_t pos;
   int samples_into_note;
-  i2sNote i2snote;
-} Channel;
+  Note notes[5];
+  int notes_len;
+  int sample_duration;
+} Song;
 
 /* piano ties together our sample table, our note reader and pico's audio buffer pool */
-#define CHAN_COUNT 4
+#define SONG_COUNT 4
 const float sound_weights[Sound_COUNT] = {
   [Sound_Sine]     = 1.00f,
   [Sound_Triangle] = 0.59f,
@@ -64,38 +61,38 @@ const float sound_weights[Sound_COUNT] = {
 static struct {
   int16_t sample_table[Sound_COUNT][TABLE_LEN];
 
-  Channel chan[CHAN_COUNT];
+  Song song[SONG_COUNT];
 
   PianoOpts opts;
 } piano_state = {0};
 
 /* type erased jerry_value_t (well, doesn't matter what as long as
  * opts.song_chars and opts.song_free work on it) */
-int piano_queue_song(void *song, double times) {
-  for (int ci = 0; ci < CHAN_COUNT; ci++) {
-    Channel *chan = piano_state.chan + ci;
-    if (!chan->active) {
-      chan->active = 1;
-      chan->times = times;
-      chan->song = song;
+int piano_queue_song(void *char_source, double times) {
+  for (int ci = 0; ci < SONG_COUNT; ci++) {
+    Song *song = piano_state.song + ci;
+    if (!song->active) {
+      song->active = 1;
+      song->times = times;
+      song->char_source = char_source;
       return 1;
     }
   }
   return 0;
 }
 
-static void piano_chan_free(Channel *chan) {
-  if (chan->song) piano_state.opts.song_free(chan->song);
-  memset(chan, 0, sizeof(Channel));
-  chan->active = 0; /* just to make sure >:) */
+static void piano_chan_free(Song *song) {
+  if (song->char_source) piano_state.opts.song_free(song->char_source);
+  memset(song, 0, sizeof(Song));
+  song->active = 0; /* just to make sure >:) */
 }
 
 int piano_unqueue_song(void *p) {
-  for (int ci = 0; ci < CHAN_COUNT; ci++) {
-    Channel *chan = piano_state.chan + ci;
+  for (int ci = 0; ci < SONG_COUNT; ci++) {
+    Song *song = piano_state.song + ci;
 
-    if (chan->song == p) {
-      piano_chan_free(chan);
+    if (song->char_source == p) {
+      piano_chan_free(song);
       return 1;
     }
   }
@@ -104,10 +101,10 @@ int piano_unqueue_song(void *p) {
 }
 
 int piano_is_song_queued(void *p) {
-  for (int ci = 0; ci < CHAN_COUNT; ci++) {
-    Channel *chan = piano_state.chan + ci;
+  for (int ci = 0; ci < SONG_COUNT; ci++) {
+    Song *song = piano_state.song + ci;
 
-    if (chan->song == p) return 1;
+    if (song->char_source == p) return 1;
   }
 
   return 0;
@@ -127,86 +124,103 @@ void piano_init(PianoOpts opts) {
     };
 
     for (int s = 0; s < Sound_COUNT; s++)
-      piano_state.sample_table[s][i] = (int)(32767 * sound_weights[s]) * soundf[s];
+      piano_state.sample_table[s][i] = (int)(soundf[s] * sound_weights[s] * 32767);
   }
 }
 
-static int16_t piano_compute_sample(Channel *chan, float *vol_used) {
-  if (!chan->active) return 0;
+static int32_t piano_compute_sample(Song *song) {
+  if (!song->active) return 0;
 
-  chan->samples_into_note++;
-  if (chan->samples_into_note >= chan->i2snote.sample_duration) {
-    chan->samples_into_note = 0;
+  song->samples_into_note++;
+  if (song->samples_into_note >= song->sample_duration) {
+    song->samples_into_note = 0;
 
-    memset(&chan->i2snote, 0, sizeof(chan->i2snote));
+    /* clear out all old notes, we got new data */
+    memset(&song->notes, 0, sizeof(song->notes));
+    song->notes_len = 0;
 
     /* pull the song into this buf so we can read next chord */
-    char song[2048] = {0};
-    if (!piano_state.opts.song_chars(chan->song, song, 2048)) {
+    char char_source[2048] = {0};
+    if (!piano_state.opts.song_chars(song->char_source, char_source, 2048)) {
       puts("song exceeds 2k chars, not playing");
       return 0;
     }
 
     while (1) {
-      if (!tune_parse(&chan->nrs, song)) {
-        chan->nrs = (NoteReadState) {0};
+      if (!tune_parse(&song->nrs, char_source)) {
+        // Song ended!
 
-        chan->times -= 1.0;
-        if (chan->times <= 0.0)
-          piano_chan_free(chan);
+        song->nrs = (NoteReadState) {0};
+        song->times -= 1.0;
+        if (song->times <= 0.0)
+          piano_chan_free(song);
 
         return 0;
       }
 
-      NrsRet *nrs_ret = &chan->nrs.ret;
+      NrsRet *nrs_ret = &song->nrs.ret;
       if (nrs_ret->kind != NrsRetKind_None) {
+        song->sample_duration = (SAMPLES_PER_SECOND / 1000) * nrs_ret->duration;
 
         /* TODO: handle chords (multiple notes per pause) */
-        if (nrs_ret->kind == NrsRetKind_Sound)
-          chan->i2snote = i2snote_sound(
-            nrs_ret->sound.freq,
-            nrs_ret->duration,
-            nrs_ret->sound.sound
-          );
+        if (nrs_ret->kind == NrsRetKind_Sound) {
+          if (song->notes_len < ARR_LEN(song->notes)) {
+            song->notes[song->notes_len++] = i2snote_sound(
+              nrs_ret->sound.freq,
+              nrs_ret->sound.sound
+            );
+          } else {
+            puts("wow too many notes");
+          }
+        }
         if (nrs_ret->kind == NrsRetKind_Pause) {
-          /* note: not overwriting entire thing. chords end with pauses,
-           * would overwrite freq and sound and play nothing */
-          chan->i2snote.sample_duration = i2snote_pause(nrs_ret->duration).sample_duration;
+          /* chords (and pauses) always end with pauses (unless something goes *horribly* wrong) */
           break;
         }
       }
     }
   }
 
-  if (chan->i2snote.step == 0)
-    return 0;
-  else {
-    int ret = (127 * piano_state.sample_table[chan->i2snote.sound][chan->pos >> 16u]) >> 8u;
+  int32_t ret = 0;
+  for (int i = 0; i < song->notes_len; i++) {
+    Note *note = song->notes + i;
+    ret += piano_state.sample_table[note->sound][note->pos >> 16u];
 
-    /* 128 is 1/2 of 256 possible vol */
-    *vol_used += 0.6f * sound_weights[chan->i2snote.sound];
-    chan->pos += chan->i2snote.step;
+    note->pos += note->step;
 
     /* wrap 'round */
-    const uint32_t pos_max = 0x10000 * TABLE_LEN;
-    if (chan->pos >= pos_max) chan->pos -= pos_max;
-
-    return ret;
+    const int32_t pos_max = 0x10000 * TABLE_LEN;
+    if (note->pos >= pos_max) note->pos -= pos_max;
   }
+  return ret;
+}
+
+int32_t remap(int32_t x, int32_t in_min, int32_t in_max, int32_t out_min, int32_t out_max) {
+  return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+}
+
+int32_t clamp(int32_t x, int32_t min, int32_t max) {
+  if (x < min) return min;
+  if (x > max) return max;
+  return x;
 }
 
 void piano_fill_sample_buf(int16_t *samples, int size) {
   /* fill buffer */
   for (int i = 0; i < size; i++) {
-    samples[i] = 0;
+    int32_t sum = 0;
+    int8_t sample_count = 0;
+    for (int ci = 0; ci < SONG_COUNT; ci++) {
+      Song *song = piano_state.song + ci;
+      if (!song->active) continue;
+      sum += piano_compute_sample(song);
+      sample_count += song->notes_len;
+    }
 
-    /* contribution from all channels cannot exceed 1.0f */
-    float vol_used = 0.0f;
-
-    for (int ci = 0; ci < CHAN_COUNT; ci++) {
-      Channel *chan = piano_state.chan + ci;
-      int vol = piano_compute_sample(chan, &vol_used);
-      if (vol_used <= 1.0f) samples[i] += vol;
+    if (sample_count != 0) {
+      samples[i] = sum/sample_count/2;
+    } else {
+      samples[i] = 0;
     }
   }
 }
