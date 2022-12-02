978d8a39c792b438d04cb3e432ae525093d10ffb
Kognise <felix.mattick@gmail.com>
2022-12-02 17:09:45 -0500

    Whoops

--- jsdev.sh
+++ jsdev.sh
@@ -1,1 +1,1 @@
-ls engine.js game.js | entr -s './tools/cstringify.py engine.js > engine.js.cstring && ./tools/cstringify.py game.js > game.js.cstring'
+ls engine.js game.js | entr -s './tools/cstringify.py engine.js > engine.js.cstring
